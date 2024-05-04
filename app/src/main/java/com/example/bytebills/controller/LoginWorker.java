92ab8f651eee3469f258d3bdf38ea463f24cc5eb
alberto <aarosteguig@gmail.com>
2024-05-04 16:29:08 +0200

    Changed from dispatcher in the worker class to different worker classes that all use remoteDBHandler to call the database and so avoiding god-object antipattern. Changed the server side to satisfy the requirements

---
+++ app/src/main/java/com/example/bytebills/controller/LoginWorker.java
@@ -0,0 +1,48 @@
+package com.example.bytebills.controller;
+
+import android.content.Context;
+
+import androidx.work.Data;
+import androidx.work.ListenableWorker;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class LoginWorker extends Worker {
+
+    public LoginWorker(Context context, WorkerParameters workerParameters) {
+        super(context, workerParameters);
+    }
+    public ListenableWorker.Result doWork() {
+        Data data = getInputData();
+        RemoteDBHandler dbHandler = new RemoteDBHandler();
+
+        String username = data.getString("username");
+        String email = data.getString("email");
+        String password = data.getString("password");
+
+        try {
+            //TODO: Decide between email and username for log in.
+            JSONObject json = new JSONObject();
+            json.put("username", username);
+            json.put("email", email);
+            json.put("password", password);
+
+            String status = dbHandler.post("login", json);
+
+            if (!status.equals("Error")) {
+                Data outputData = new Data.Builder()
+                        .putString("status", status)
+                        .build();
+                return ListenableWorker.Result.success(outputData);
+            } else {
+                return ListenableWorker.Result.failure();
+            }
+
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
