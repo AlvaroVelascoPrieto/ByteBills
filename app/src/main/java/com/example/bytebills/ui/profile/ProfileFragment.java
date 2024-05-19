fc280976a1e938b92b42c5351055772cd9ccf3e8
AlbertoArostegui <aarosteguig@gmail.com>
2024-05-19 12:19:01 +0200

    New profile view

--- app/src/main/java/com/example/bytebills/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/bytebills/ui/profile/ProfileFragment.java
@@ -4,34 +4,57 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkManager;
 
+import com.example.bytebills.R;
+import com.example.bytebills.controller.UserProfileWorker;
+import com.example.bytebills.databinding.FragmentProfileBinding;
 import com.example.bytebills.databinding.FragmentSlideshowBinding;
-
 public class ProfileFragment extends Fragment {
 
-    private FragmentSlideshowBinding binding;
-
+    private FragmentProfileBinding binding;
+    private TextView textViewUsername;
+    private TextView textViewEmail;
+    private TextView textViewAssetCount;
+    private TextView textViewAvgValue;
+    private TextView textViewTotalValue;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        ProfileViewModel slideshowViewModel =
-                new ViewModelProvider(this).get(ProfileViewModel.class);
 
-        binding = FragmentSlideshowBinding.inflate(inflater, container, false);
+        binding = FragmentProfileBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textSlideshow;
-        slideshowViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
-    }
+        textViewUsername = binding.tvUsername;
+        textViewEmail = binding.tvEmail;
+        textViewTotalValue = binding.tvTotalValue;
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
+        OneTimeWorkRequest profileWork = new OneTimeWorkRequest.Builder(UserProfileWorker.class).build();
+        WorkManager.getInstance(requireActivity()).getWorkInfoByIdLiveData(profileWork.getId())
+                .observe(getViewLifecycleOwner(), workInfo -> {
+            if (workInfo != null && workInfo.getState().isFinished()) {
+                workInfo.getOutputData().getString("username");
+                workInfo.getOutputData().getString("email");
+                workInfo.getOutputData().getInt("asset_count", 0);
+                workInfo.getOutputData().getString("avg_value");
+                workInfo.getOutputData().getString("total_value");
+                workInfo.getOutputData().getString("most_common_asset");
+                workInfo.getOutputData().getStringArray("top_3_assets");
+
+                textViewUsername.setText(workInfo.getOutputData().getString("username"));
+                textViewEmail.setText(workInfo.getOutputData().getString("email"));
+                textViewTotalValue.setText(workInfo.getOutputData().getString("total_value"));
+
+            }
+        });
+        WorkManager.getInstance(requireActivity()).enqueue(profileWork);
+
+        return root;
     }
 }
