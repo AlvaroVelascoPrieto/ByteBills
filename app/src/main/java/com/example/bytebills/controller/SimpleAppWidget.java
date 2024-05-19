9150cffefca9410cfee3eb16963d628d637b3a01
AlvaroVelascoPrieto <alvaritovelasco2002@gmail.com>
2024-05-19 14:12:04 +0200

    Widget Creation

--- app/src/main/java/com/example/bytebills/controller/SimpleAppWidget.java
+++ app/src/main/java/com/example/bytebills/controller/SimpleAppWidget.java
@@ -1,20 +1,16 @@
 package com.example.bytebills.controller;
-import android.annotation.SuppressLint;
-import android.annotation.TargetApi;
+
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
+import android.net.Uri;
 import android.widget.RemoteViews;
 
-import androidx.annotation.NonNull;
-
-import com.example.bytebills.MainActivity;
 import com.example.bytebills.R;
 
-public class StockWidgetProvider extends AppWidgetProvider {
+public class SimpleAppWidget extends AppWidgetProvider {
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
@@ -27,7 +23,16 @@ public class StockWidgetProvider extends AppWidgetProvider {
     private void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                  int appWidgetId) {
 
-        //UPDATECODE
+
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.simple_app_widget);
+
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://albert.com"));
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+        views.setOnClickPendingIntent(R.id.tvWidget, pendingIntent);
+
+        appWidgetManager.updateAppWidget(appWidgetId, views);
     }
 
 }
